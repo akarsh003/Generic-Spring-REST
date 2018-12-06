052e6759906ebba4f4a99b45f0559da1108b3cc0
Akarsha <akarshakr2000@gmail.com>
2018-12-06 19:10:47 +0530

    Generic

--- src/main/java/com/example/demo/employee/EmplRepo.java
+++ src/main/java/com/example/demo/employee/EmplRepo.java
@@ -10,12 +10,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
+//import org.springframework.data.repository.query.Param;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//import org.springframework.data.rest.core.annotation.RestResource;
 //import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 
 //import com.example.demo.model.Department;
 import com.example.demo.model.Employee;
