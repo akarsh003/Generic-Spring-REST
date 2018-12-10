1f1e532e663c828b9f62a8b18f2f54109a63b72d
Akarsha <akarshakr2000@gmail.com>
2018-12-10 12:11:52 +0530

    Fixed merge issues

--- src/main/java/com/example/demo/employee/EmployeeController.java
+++ src/main/java/com/example/demo/employee/EmployeeController.java
@@ -5,13 +5,9 @@ import com.example.demo.department.DepartmentRecord;
 import com.example.demo.model.Department;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 
 import java.net.URISyntaxException;
 
-=======
-import java.net.URISyntaxException;
->>>>>>> 175b6840b779bc523a40c384a78c94f0c84b9473
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -43,7 +39,6 @@ import java.util.Optional;
 
 @RequestMapping(EmployeeController.URI)
 @RestController
-<<<<<<< HEAD
 public class EmployeeController extends GenericController<Employee, Integer> {
 
 	static final String URI = "/employee";
@@ -93,56 +88,4 @@ public class EmployeeController extends GenericController<Employee, Integer> {
 		super.deleteImpl(id);
 		return super.noContent(id);
 	}
-=======
-@CrossOrigin()
-public class EmployeeController extends GenericController<Employee, Integer> {
-
-
-    static final String URI = "/api/employee";
-
-    //Calling generic controller by passing department repository
-
-    @Autowired
-    public EmployeeController(EmployeeRepo repo) {
-        super(URI, repo);
-
-    }
-
-    @RequestMapping
-    public ResponseEntity<Page<Employee>> all(Pageable pageable, @RequestParam(value = "search", required = false) String search) {
-        return super.listResponse(super.allImpl(pageable, search), pageable, search);
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    protected ResponseEntity<Employee> get(Integer id) {
-        return super.respondToGet(id, super.getImpl(id));
-    }
-
-    @RequestMapping(method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
-    protected ResponseEntity<Employee> create(Employee employee) throws URISyntaxException {
-        if (employee.getId() != 0) {
-            return super.cannotCreateEntityWithKey();
-        }
-        Employee result = super.createImpl(employee);
-        return super.created(result.getId(),result);
-
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = {MediaType.APPLICATION_JSON_VALUE})
-    protected ResponseEntity<Employee> update(Integer id, Employee employee) {
-        return super.getImpl(id)
-                .map(oldEmployee -> {
-                    Employee updatedEntity = super.updateImpl(oldEmployee, employee);
-                    return super.updated(id, updatedEntity);
-                })
-                .orElse(super.updateNotFound(id));
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    protected ResponseEntity<Employee>  delete(Integer id) {
-        super.deleteImpl(id);
-        return super.noContent(id);
-    }
-
->>>>>>> 175b6840b779bc523a40c384a78c94f0c84b9473
 }
